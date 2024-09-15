0d9c2da8d919e91054bc280d0c6e060b1452f4c0
choiseoji <choiseoji123@naver.com>
2024-09-16 00:13:37 +0900

    [feat]: 댓글 domain 작성

---
+++ src/main/java/com/ceos20/instagram/comment/domain/Comment.java
@@ -0,0 +1,40 @@
+package com.ceos20.instagram.comment.domain;
+
+import com.ceos20.instagram.post.domain.Post;
+import com.ceos20.instagram.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parentComment;
+
+    @OneToMany(mappedBy = "parentComment")
+    private List<Comment> childrenComment = new ArrayList<>();
+}
