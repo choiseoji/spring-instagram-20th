f238e687794aea9efadec9a890c1a581faffeb27
choiseoji <choiseoji123@naver.com>
2024-09-16 00:14:30 +0900

    [feat]: 게시글 좋아요 domain 작성

---
+++ src/main/java/com/ceos20/instagram/postLike/domain/PostLike.java
@@ -0,0 +1,25 @@
+package com.ceos20.instagram.postLike.domain;
+
+import com.ceos20.instagram.post.domain.Post;
+import com.ceos20.instagram.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
