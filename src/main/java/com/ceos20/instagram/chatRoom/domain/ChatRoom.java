645cf89b631cfd0cf8a0b77b07e261656de90f7c
choiseoji <choiseoji123@naver.com>
2024-09-16 00:13:28 +0900

    [feat]: 채팅방 domain 작성

---
+++ src/main/java/com/ceos20/instagram/chatRoom/domain/ChatRoom.java
@@ -0,0 +1,24 @@
+package com.ceos20.instagram.chatRoom.domain;
+
+import com.ceos20.instagram.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user1_id")
+    private User user1;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user2_id")
+    private User user2;
+}
