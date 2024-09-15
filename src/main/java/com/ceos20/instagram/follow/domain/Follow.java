e65b9eafda756dc252e53943caff7781a6dda3f5
choiseoji <choiseoji123@naver.com>
2024-09-16 00:13:48 +0900

    [feat]: 팔로우, 팔로잉 domain 작성

---
+++ src/main/java/com/ceos20/instagram/follow/domain/Follow.java
@@ -0,0 +1,24 @@
+package com.ceos20.instagram.follow.domain;
+
+import com.ceos20.instagram.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "follow_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id")
+    private User follower;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id")
+    private User following;
+}
