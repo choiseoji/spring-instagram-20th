094e16428f93184bd914c1fd8b91d85fe9a0777a
choiseoji <choiseoji123@naver.com>
2024-09-16 00:13:59 +0900

    [feat]: 사진 domain 작성

---
+++ src/main/java/com/ceos20/instagram/image/domain/Image.java
@@ -0,0 +1,24 @@
+package com.ceos20.instagram.image.domain;
+
+import com.ceos20.instagram.post.domain.Post;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+
+    private String imageUrl;
+
+    private Long index;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
