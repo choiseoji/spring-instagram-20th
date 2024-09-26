feb1e642b101e5791b98c95eee6be3ddae0d992b
choiseoji <choiseoji123@naver.com>
2024-09-27 02:02:41 +0900

    [feat]: JpaRepository로 변경

--- src/main/java/com/ceos20/instagram/follow/repository/FollowRepository.java
+++ src/main/java/com/ceos20/instagram/follow/repository/FollowRepository.java
@@ -1,21 +1,7 @@
 package com.ceos20.instagram.follow.repository;
 
 import com.ceos20.instagram.follow.domain.Follow;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-@Repository
-@RequiredArgsConstructor
-public class FollowRepository {
-
-    private final EntityManager em;
-
-    // follow 저장
-    public void save(Follow follow) {
-        em.persist(follow);
-    }
-
+public interface FollowRepository extends JpaRepository<Follow, Long> {
 }
--- src/main/java/com/ceos20/instagram/user/repository/UserRepository.java
+++ src/main/java/com/ceos20/instagram/user/repository/UserRepository.java
@@ -1,31 +1,7 @@
 package com.ceos20.instagram.user.repository;
 
 import com.ceos20.instagram.user.domain.User;
-import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-@RequiredArgsConstructor
-public class UserRepository {
-
-    private final EntityManager em;
-
-    // user 저장
-    public void save(User user) {
-        em.persist(user);
-    }
-
-    // id로 회원 조회
-    public User findById(Long id) {
-        return em.find(User.class, id);
-    }
-
-    // nickname으로 회원 조회
-    public User findByNickname(String nickname) {
-        return em.createQuery("select u from User u where u.nickname = :nickname", User.class)
-                .setParameter("nickname", nickname)
-                .getSingleResult();
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
 }
