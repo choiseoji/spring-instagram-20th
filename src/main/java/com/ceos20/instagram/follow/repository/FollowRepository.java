8793c65b1be54247e32258781b90d839718fdd77
choiseoji <choiseoji123@naver.com>
2024-09-27 12:20:58 +0900

    [feat]: 내가 팔로우하는 사람, 나를 팔로우하는 사람 리스트 반환하는 서비스 코드 작성

--- src/main/java/com/ceos20/instagram/follow/repository/FollowRepository.java
+++ src/main/java/com/ceos20/instagram/follow/repository/FollowRepository.java
@@ -1,7 +1,20 @@
 package com.ceos20.instagram.follow.repository;
 
 import com.ceos20.instagram.follow.domain.Follow;
+import com.ceos20.instagram.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+    // toUser를 팔로우하는 사람들(fromUser) 리스트 반환
+    @Query("SELECT f.fromUser FROM Follow f WHERE f.toUser = :toUser")
+    List<User> findByToUser(@Param("toUser") User toUser);
+
+    // fromUser가 팔로우하는 사람들(toUser) 리스트 반환
+    @Query("SELECT f.toUser FROM Follow f WHERE f.fromUser = :fromUser")
+    List<User> findByFromUser(@Param("fromUser") User fromUser);
 }
--- src/main/java/com/ceos20/instagram/follow/service/FollowService.java
+++ src/main/java/com/ceos20/instagram/follow/service/FollowService.java
@@ -1,11 +1,16 @@
 package com.ceos20.instagram.follow.service;
 
 import com.ceos20.instagram.follow.domain.Follow;
+import com.ceos20.instagram.follow.dto.GetFollowerResponse;
+import com.ceos20.instagram.follow.dto.GetFollowingResponse;
 import com.ceos20.instagram.follow.repository.FollowRepository;
 import com.ceos20.instagram.user.domain.User;
 import com.ceos20.instagram.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +30,37 @@ public class FollowService {
                 .toUser(friend)
                 .build();
         followRepository.save(follow);
+    }
+
+    // 나를 팔로우하는 사람들 리스트 반환
+    public List<GetFollowerResponse> getFollower(User user) {
+
+        List<User> findFollowers = followRepository.findByToUser(user);
+
+        List<GetFollowerResponse> followerResponses = new ArrayList<>();
+        for (User follower : findFollowers) {
+            GetFollowerResponse followerResponse = GetFollowerResponse.builder()
+                    .userId(follower.getId())
+                    .nickname(follower.getNickname())
+                    .build();
+            followerResponses.add(followerResponse);
+        }
+        return followerResponses;
+    }
+
+    // 내가 팔로우하는 사람들 리스트 반환
+    public List<GetFollowingResponse> getFollowing(User user) {
+
+        List<User> findFollowings = followRepository.findByFromUser(user);
+
+        List<GetFollowingResponse> followingResponses = new ArrayList<>();
+        for (User following : findFollowings) {
+            GetFollowingResponse followingResponse = GetFollowingResponse.builder()
+                    .userId(following.getId())
+                    .nickname(following.getNickname())
+                    .build();
+            followingResponses.add(followingResponse);
+        }
+        return followingResponses;
     }
 }
