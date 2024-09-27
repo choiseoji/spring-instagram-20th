e3228fe500b5b0173c0011f513f2ab955e14e24b
choiseoji <choiseoji123@naver.com>
2024-09-27 10:34:50 +0900

    [feat]: userId와 postId로 본문 좋아요 조회하는 repository 코드 추가

--- src/main/java/com/ceos20/instagram/postLike/repository/PostLikeRepository.java
+++ src/main/java/com/ceos20/instagram/postLike/repository/PostLikeRepository.java
@@ -3,5 +3,9 @@ package com.ceos20.instagram.postLike.repository;
 import com.ceos20.instagram.postLike.domain.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+
+    Optional<PostLike> findByUserIdAndPostId(Long userId, Long postId);
 }
