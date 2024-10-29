4ac33f0574640579f4efd7e65dbd780c87f7d0ea
choiseoji <choiseoji123@naver.com>
2024-10-30 00:08:12 +0900

    [feat]: 게시글 작성 api 추가

---
+++ src/main/java/com/ceos20/instagram/post/controller/PostController.java
@@ -0,0 +1,30 @@
+package com.ceos20.instagram.post.controller;
+
+import com.ceos20.instagram.global.annotation.Login;
+import com.ceos20.instagram.member.domain.Member;
+import com.ceos20.instagram.post.dto.CreatePostRequest;
+import com.ceos20.instagram.post.service.PostService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/post")
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/")
+    public ResponseEntity createPost(
+            @RequestBody CreatePostRequest createPostRequest,
+            @Login Member member) {
+
+        postService.createPost(createPostRequest, member);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
