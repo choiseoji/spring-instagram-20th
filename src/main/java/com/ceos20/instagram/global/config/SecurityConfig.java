903aac7a8ed3ae6184d86fb4e418f60507ac374b
choiseoji <choiseoji123@naver.com>
2024-11-08 00:57:26 +0900

    [feat]: 401, 403 에러 관련 시큐리티 설정 추가

--- src/main/java/com/ceos20/instagram/global/config/SecurityConfig.java
+++ src/main/java/com/ceos20/instagram/global/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ceos20.instagram.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,13 +16,34 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/login").permitAll()
+        // CSRF 비활성화
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // STATELESS
+        http.sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // 요청 URI별 권한 설정
+        http.authorizeHttpRequests((authorize) ->
+                authorize.requestMatchers("/login").permitAll()
                         .anyRequest().authenticated());
+
+        // 401, 403 에러 처리
+        http.exceptionHandling(exceptionHandling -> exceptionHandling
+                    .authenticationEntryPoint((request, response, authException) -> {
+                        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                        response.setContentType("application/json");
+                        response.setCharacterEncoding("UTF-8");
+                        response.getWriter().write("[ERROR 401] 인증되지 않은 사용자입니다.");
+                    })
+                    .accessDeniedHandler((request, response, accessDeniedException) -> {
+                        response.setStatus(HttpStatus.FORBIDDEN.value());
+                        response.setContentType("application/json");
+                        response.setCharacterEncoding("UTF-8");
+                        response.getWriter().write("[ERROR 403] 권한이 없는 사용자입니다.");
+                    }));
+
+        // JWT 관련 처리
 
         return http.build();
     }
