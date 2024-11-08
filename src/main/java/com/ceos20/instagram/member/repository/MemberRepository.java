0387895210763b84773ac4a08da9a161dce258cc
choiseoji <choiseoji123@naver.com>
2024-11-08 22:10:21 +0900

    [feat]: memberRepository 메서드 추가

--- src/main/java/com/ceos20/instagram/member/repository/MemberRepository.java
+++ src/main/java/com/ceos20/instagram/member/repository/MemberRepository.java
@@ -3,5 +3,13 @@ package com.ceos20.instagram.member.repository;
 import com.ceos20.instagram.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Boolean existsByNickname(String nickname);
+
+    Boolean existsByEmail(String email);
+
+    Optional<Member> findByNickname(String nickname);
 }
