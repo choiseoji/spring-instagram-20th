b22ca6bc70a157268dc4cc90128dd9145e77999f
choiseoji <choiseoji123@naver.com>
2024-11-08 22:11:14 +0900

    [feat]: passwordEncoder 와 authenticationManager 추가

--- src/main/java/com/ceos20/instagram/global/config/SecurityConfig.java
+++ src/main/java/com/ceos20/instagram/global/config/SecurityConfig.java
@@ -3,15 +3,30 @@ package com.ceos20.instagram.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .build();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,7 +40,8 @@ public class SecurityConfig {
 
         // 요청 URI별 권한 설정
         http.authorizeHttpRequests((authorize) ->
-                authorize.requestMatchers("/login").permitAll()
+                authorize.requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated());
 
         // 401, 403 에러 처리
