4a58d93fe62c3a989e4aeab267e9048b7cb6946a
choiseoji <choiseoji123@naver.com>
2024-11-09 00:17:54 +0900

    [feat]: 회원가입, 로그인 서비스 코드 추가

---
+++ src/main/java/com/ceos20/instagram/auth/service/AuthService.java
@@ -0,0 +1,50 @@
+package com.ceos20.instagram.auth.service;
+
+import com.ceos20.instagram.auth.dto.SignInRequest;
+import com.ceos20.instagram.auth.dto.SignUpRequest;
+import com.ceos20.instagram.global.exception.ConflictException;
+import com.ceos20.instagram.global.exception.ExceptionCode;
+import com.ceos20.instagram.global.security.JwtTokenProvider;
+import com.ceos20.instagram.global.security.dto.JwtToken;
+import com.ceos20.instagram.member.domain.Member;
+import com.ceos20.instagram.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+
+    private final MemberService memberService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Transactional
+    public void signUp(SignUpRequest signUpRequest) {
+
+        if (memberService.nicknameAlreadyExists(signUpRequest.getNickname()))
+            throw new ConflictException(ExceptionCode.ALREADY_EXISTS_NICKNAME);
+
+        if (memberService.emailAlreadyExists(signUpRequest.getEmail()))
+            throw new ConflictException(ExceptionCode.ALREADY_EXISTS_MEMBER);
+
+        memberService.saveMember(signUpRequest);
+    }
+
+    @Transactional
+    public JwtToken signIn(SignInRequest signInRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        signInRequest.getNickname(),
+                        signInRequest.getPassword()));
+
+        Member member = memberService.getMemberByNickname(signInRequest.getNickname());
+        return jwtTokenProvider.getToken(member);
+    }
+}
