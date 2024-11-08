e55b51424ba34117689cfcff0c679beac0924197
choiseoji <choiseoji123@naver.com>
2024-11-08 22:06:25 +0900

    [feat]: 409 에러 추가

--- src/main/java/com/ceos20/instagram/global/exception/ExceptionCode.java
+++ src/main/java/com/ceos20/instagram/global/exception/ExceptionCode.java
@@ -6,13 +6,18 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionCode {
 
+    // 404
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "해당 member는 존재하지 않습니다."),
     NOT_FOUND_POST(HttpStatus.NOT_FOUND, "해당 post는 존재하지 않습니다."),
     NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, "해당 comment는 존재하지 않습니다."),
     NOT_FOUND_IMAGE(HttpStatus.NOT_FOUND, "해당 image는 존재하지 않습니다."),
     NOT_FOUND_POSTLIKE(HttpStatus.NOT_FOUND, "해당 postlike는 존재하지 않습니다."),
     NOT_FOUND_CHATROOM(HttpStatus.NOT_FOUND, "해당 chatroom은 존재하지 않습니다."),
-    NOT_FOUND_MESSAGE(HttpStatus.NOT_FOUND, "해당 message는 존재하지 않습니다.");
+    NOT_FOUND_MESSAGE(HttpStatus.NOT_FOUND, "해당 message는 존재하지 않습니다."),
+
+    // 409
+    ALREADY_EXISTS_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 nickname 입니다."),
+    ALREADY_EXISTS_MEMBER(HttpStatus.CONFLICT, "이미 존재하는 member 입니다.");
 
     private final HttpStatus status;
     private final String message;
