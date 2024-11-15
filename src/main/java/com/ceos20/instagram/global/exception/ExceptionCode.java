b7a947aea4df5a64ede50d212860481e5982d550
choiseoji <choiseoji123@naver.com>
2024-11-16 01:13:42 +0900

    [feat]: exceptionCode에서 400, 401 에러 코드 추가

--- src/main/java/com/ceos20/instagram/global/exception/ExceptionCode.java
+++ src/main/java/com/ceos20/instagram/global/exception/ExceptionCode.java
@@ -6,6 +6,14 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionCode {
 
+    // 400
+    MALFORMED_TOKEN(HttpStatus.BAD_REQUEST, "잘못된 JWT 형식입니다."),
+    ILLEGAL_ARGUMENT(HttpStatus.BAD_REQUEST, "토큰이 비어있거나 잘못된 형식입니다."),
+
+    // 401
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    INVALID_SIGNATURE(HttpStatus.UNAUTHORIZED, "서명이 유효하지 않습니다."),
+
     // 404
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "해당 member는 존재하지 않습니다."),
     NOT_FOUND_POST(HttpStatus.NOT_FOUND, "해당 post는 존재하지 않습니다."),
